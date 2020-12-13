1c37db3edf91fc6f966c9e4b9ad82ec3e8c6c4c9
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-13 19:11:30 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,14 @@
 def using_until
   levitation_force = 6
   #your code here
+  while levitation_force < 10
+  levitation_force += 1
+  puts "Wingardium Leviosa"
+    counter = 0
+until counter == 10
+  puts "Wingardium Leviosa"
+  counter += 1
+end 
     
+  end
 end
-
